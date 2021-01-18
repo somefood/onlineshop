43ed0458dbfd56dc606db80413e8ffd8e867ccab
Seokju Hong <iamseokjuhong@gmail.com>
2021-01-18 10:22:17 +0900

    10 admin 페이지 설저어

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'category', 'price', 'stock', 'available_display',
+                    'available_order', 'created', 'updated']
+    list_filter = ['available_display', 'created', 'updated', 'category']
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['price', 'stock' 'available_display', 'available_order']
