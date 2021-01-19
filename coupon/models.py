cf74f6ba9ddc9d02429a6d973aeb2bb60657331e
Seokju Hong <iamseokjuhong@gmail.com>
2021-01-19 11:18:36 +0900

    25 관리자 페이지 쿠폰 등록, 장바구니 적용해보기

--- coupon/models.py
+++ coupon/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     use_from = models.DateTimeField()
     use_to = models.DateTimeField()
-    amount = models.IntegerField(validators=[MinLengthValidator(0), MaxLengthValidator(100000)])
+    amount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100000)])
     active = models.BooleanField()
 
     def __str__(self):
