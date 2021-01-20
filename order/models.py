e110e9e393d8098f81eccfb58c7ca6eea0ce50c2
Seokju Hong <iamseokjuhong@gmail.com>
2021-01-20 10:20:34 +0900

    31 OrderItem Model 생성

--- order/models.py
+++ order/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from coupon.models import Coupon
-
+from shop.models import Product
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -28,3 +28,16 @@ class Order(models.Model):
 
     class Meta:
         ordering = ['-created']
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.id}"
+
+    def get_item_price(self):
+        return self.price * self.quantity
