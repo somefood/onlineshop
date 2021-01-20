ce205100b9b5f00c4852fceef8f297280d244a96
Seokju Hong <iamseokjuhong@gmail.com>
2021-01-20 15:45:21 +0900

    38 주문 완료 페이지 설정

--- order/views.py
+++ order/views.py
@@ -13,7 +13,8 @@ def order_create(request):
             order = form.save()
             if cart.coupon:
                 order.coupon = cart.coupon
-                order.discount = cart.coupon.amout
+                # order.discount = cart.coupon.amout
+                order.discount = cart.get_discount_total()
                 order.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], qunatity=item['quantity'])
@@ -24,3 +25,11 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'order/create.html', {'cart': cart, 'form': form})
+
+
+# JS 동작하지 않는 환경에서도 주문은 가능해야한다.
+def order_complete(request):
+    order_id = request.GET.get('order_id')
+    # order = Order.objects.get(id=order_id)
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'order/created.html', {'order': order})
