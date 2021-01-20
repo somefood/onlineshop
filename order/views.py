d3e914e91137a4c4c12e2adb3aac149ce07d3dcb
Seokju Hong <iamseokjuhong@gmail.com>
2021-01-20 15:37:38 +0900

    36 Order view 생성

--- order/views.py
+++ order/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from cart.cart import Cart
+from .forms import OrderCreateForm
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST":
+        # 입력받은 정보를 후처리
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amout
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], qunatity=item['quantity'])
+            cart.clear()
+            return render(request, 'order/created.html', {'order': order})
+        else:
+            pass
+    else:
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', {'cart': cart, 'form': form})
